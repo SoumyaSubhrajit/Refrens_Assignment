5fef4dfb09c51bfbc0cb6604f80b8619977d6929
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-07-22 13:12:20 +0530

    Adding  the code.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,11 @@
 const experss = require('express');
 const router = express.Router();
 const authRoutes = require('./authRoutes');
+
 const tickets = require('./ticketRoutes');
 
 
 router.use('/auth', authRoutes);
 router.use('/tickets', tickets);
+
+module.exports = router;
