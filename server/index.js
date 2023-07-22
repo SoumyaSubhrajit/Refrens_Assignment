2562bcd71a79de1ab293816e190b2ed19b4cd59f
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-07-22 15:32:58 +0530

    Adding some code in the codebase.

--- server/index.js
+++ server/index.js
@@ -20,17 +20,30 @@ app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan('combined'));
 
+
+// Use the routes from the router.
+app.use('/api', routes);
+
+// 404 Error handling.
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Not found' })
+});
+
+app.use(errorHandler);
+
+
+
 // register API endpoint..
-app.post('/register',)
+// app.post('/register',)
 
 // Verify email API endpoint
-app.get('/verify-email',);
+// app.get('/verify-email',);
 
 
 
 
 // lOGIN API endpoint.
-app.post('/login',);
+// app.post('/login',);
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,4 @@
-const experss = require('express');
+const express = require('express');
 const router = express.Router();
 const authRoutes = require('./authRoutes');
 
